149f655a774db8686ebdbe9bc833dfc12f545439
Oskar <case112@users.noreply.github.com>
2020-09-26 16:29:50 +0300

    Registering Django Models with Wagtails ModelAdmin

---
+++ subscribers/admin.py
@@ -0,0 +1,19 @@
+from wagtail.contrib.modeladmin.options import(
+    ModelAdmin,
+    modeladmin_register,
+)
+from .models import Subscribers
+
+class SubscriberAdmin(ModelAdmin):
+    """ Subscriber admin """
+
+    model = Subscribers
+    menu_label = 'Subscribers'
+    menu_icon = 'user'
+    menu_order = 290
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ('email', 'full_name',)
+    search_fields = ('email', 'full_name',)
+
+modeladmin_register(SubscriberAdmin)
