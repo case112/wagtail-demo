b07fd1588e3cd16c6f889e27ad97d8479895104e
Oskar <case112@users.noreply.github.com>
2020-10-05 20:08:16 +0300

    Using a SnippetChooserPanel to Select Multiple Blog Authors

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,37 @@
 from django.db import models
 from django.shortcuts import render
 
-
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel
-from wagtail.core.models import Page 
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    StreamFieldPanel,
+    MultiFieldPanel,
+    InlinePanel,
+)
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import StreamField
+
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 from wagtail.snippets.models import register_snippet
 
 from streams import blocks 
+
+
+class BlogAuthorOrderable(Orderable):
+    """ This allows us to select one or more authors to a post """
+
+    page = ParentalKey('blog.BlogDetailPage', related_name='blog_authors')
+    author = models.ForeignKey(
+        'blog.BlogAuthor',
+        on_delete=models.CASCADE,
+    )
+
+    panels = [
+        SnippetChooserPanel('author'),
+    ]
+
 
 
 class BlogAuthor(models.Model):
@@ -127,4 +149,10 @@ class BlogDetailPage(Page):
         FieldPanel('custom_title'),
         ImageChooserPanel('blog_image'),
         StreamFieldPanel('content'),
+        MultiFieldPanel(
+            [
+                InlinePanel('blog_authors', label='Author', min_num=1, max_num=4)
+            ],
+            heading='Authors'
+        ),
     ]
